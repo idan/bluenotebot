7cc93fc8cd343ee2d67a2ab7e037d90f1a4daad0
Idan Gazit <idan@pixane.net>
2009-07-13 05:15:00 +0300

    Added home view

--- bluenote/urls.py
+++ bluenote/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
-from bluenote.views import search
+from bluenote.views import search, home
 
 urlpatterns = patterns('',
     url(r'search/(?P<terms>[\w,]+)', search, name='search'),
+    url(r'^$', home, name='home')
 )
--- bluenote/views.py
+++ bluenote/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from bluenote.models import DocEntry
+from bluenote.forms import SearchForm
+import re
+
+patt_split = re.compile(r'[\s,]')
 
 def search(request, terms):
     """Show a list of search results for a list of terms."""
@@ -32,4 +38,16 @@ def search(request, terms):
         context_instance=RequestContext(request))
 
 def home(request):
-    pass
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            terms = [t for t in patt_split.split(form.cleaned_data['terms']) if t != '']
+            redirect_to = reverse('search', kwargs={'terms':','.join(terms)})
+            return HttpResponseRedirect(redirect_to)
+    else:
+        form = SearchForm()
+    
+    return render_to_response(
+        'bluenote/home.html',
+        {'form':form},
+        context_instance=RequestContext(request))
